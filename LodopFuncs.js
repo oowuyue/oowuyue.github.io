a9c979c7b1bbb0642c210c7bddcdfcc6ed038b3b
oowuyue <851616860@qq.com>
2022-05-18 10:13:16 +0800

    Update LodopFuncs.js

--- LodopFuncs.js
+++ LodopFuncs.js
@@ -1,4 +1,4 @@
-//==本JS是加载Lodop插件及CLodop服务的综合示例，可直接使用，建议看懂后融进自己页面程序==
+//==本JS是加载Lodop插件或Web打印服务CLodop/Lodop7的综合示例，可直接使用，建议理解后融入自己程序==
 
 var CreatedOKLodopObject, CLodopIsLocal, CLodopJsState;
 
@@ -6,36 +6,28 @@ var CreatedOKLodopObject, CLodopIsLocal, CLodopJsState;
 function needCLodop() {
     try {
         var ua = navigator.userAgent;
-        if (ua.match(/Windows\sPhone/i))
-            return true;
-        if (ua.match(/iPhone|iPod|iPad/i))
-            return true;
-        if (ua.match(/Android/i))
-            return true;
-        if (ua.match(/Edge\D?\d+/i))
-            return true;
+        if (ua.match(/Windows\sPhone/i))   return true;
+        if (ua.match(/iPhone|iPod|iPad/i)) return true;
+        if (ua.match(/Android/i))          return true;
+        if (ua.match(/Edge\D?\d+/i))       return true;
 
         var verTrident = ua.match(/Trident\D?\d+/i);
         var verIE = ua.match(/MSIE\D?\d+/i);
         var verOPR = ua.match(/OPR\D?\d+/i);
         var verFF = ua.match(/Firefox\D?\d+/i);
         var x64 = ua.match(/x64/i);
-        if ((!verTrident) && (!verIE) && (x64))
-            return true;
+        if ((!verTrident) && (!verIE) && (x64)) return true;
         else if (verFF) {
             verFF = verFF[0].match(/\d+/);
-            if ((verFF[0] >= 41) || (x64))
-                return true;
+            if ((verFF[0] >= 41) || (x64)) return true;
         } else if (verOPR) {
             verOPR = verOPR[0].match(/\d+/);
-            if (verOPR[0] >= 32)
-                return true;
+            if (verOPR[0] >= 32) return true;
         } else if ((!verTrident) && (!verIE)) {
             var verChrome = ua.match(/Chrome\D?\d+/i);
             if (verChrome) {
                 verChrome = verChrome[0].match(/\d+/);
-                if (verChrome[0] >= 41)
-                    return true;
+                if (verChrome[0] >= 41) return true;
             }
         }
         return false;
@@ -44,15 +36,22 @@ function needCLodop() {
     }
 }
 
-//==加载引用CLodop的主JS,用双端口8000和18000(以防其中一个被占):==
+//加载CLodop时用双端口(http是8000/18000,而https是8443/8444)以防其中某端口被占,
+//主JS文件名“CLodopfuncs.js”是固定名称，其内容是动态的，与其链接的打印环境有关:
 function loadCLodop() {
     if (CLodopJsState == "loading" || CLodopJsState == "complete") return;
     CLodopJsState = "loading";
     var head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
     var JS1 = document.createElement("script");
     var JS2 = document.createElement("script");
-    JS1.src = "http://localhost:8000/CLodopfuncs.js?priority=1";   //https://localhost.lodop.net:8443/CLodopfuncs.js?priority=1
-    JS2.src = "http://localhost:18000/CLodopfuncs.js";
+
+    if (window.location.protocol=='https:') {
+      JS1.src = "https://localhost.lodop.net:8443/CLodopfuncs.js";
+      JS2.src = "https://localhost.lodop.net:8444/CLodopfuncs.js";
+    } else {
+      JS1.src = "http://localhost:8000/CLodopfuncs.js";
+      JS2.src = "http://localhost:18000/CLodopfuncs.js";
+    }
     JS1.onload  = JS2.onload  = function()    {CLodopJsState = "complete";}
     JS1.onerror = JS2.onerror = function(evt) {CLodopJsState = "complete";}
     head.insertBefore(JS1, head.firstChild);
@@ -60,49 +59,58 @@ function loadCLodop() {
     CLodopIsLocal = !!((JS1.src + JS2.src).match(/\/\/localho|\/\/127.0.0./i));
 }
 
-if (needCLodop()){loadCLodop();}//加载
+if (needCLodop()){loadCLodop();}//开始加载
 
 //==获取LODOP对象主过程,判断是否安装、需否升级:==
 function getLodop(oOBJECT, oEMBED) {
-    var strHtmInstall = "<br><font color='#FF00FF'>打印控件未安装!点击这里<a href='install_lodop32.zip' target='_self'>执行安装</a>,安装后请刷新页面或重新进入。</font>";
-    var strHtmUpdate = "<br><font color='#FF00FF'>打印控件需要升级!点击这里<a href='install_lodop32.zip' target='_self'>执行升级</a>,升级后请重新进入。</font>";
-    var strHtm64_Install = "<br><font color='#FF00FF'>打印控件未安装!点击这里<a href='install_lodop64.zip' target='_self'>执行安装</a>,安装后请刷新页面或重新进入。</font>";
-    var strHtm64_Update = "<br><font color='#FF00FF'>打印控件需要升级!点击这里<a href='install_lodop64.zip' target='_self'>执行升级</a>,升级后请重新进入。</font>";
-    var strHtmFireFox = "<br><br><font color='#FF00FF'>（注意：如曾安装过Lodop旧版附件npActiveXPLugin,请在【工具】->【附加组件】->【扩展】中先卸它）</font>";
-    var strHtmChrome = "<br><br><font color='#FF00FF'>(如果此前正常，仅因浏览器升级或重安装而出问题，需重新执行以上安装）</font>";
-    var strCLodopInstall_1 = "<br><font color='#FF00FF'>Web打印服务CLodop未安装启动，点击这里<a href='CLodop_Setup_for_Win32NT.zip' target='_self'>下载执行安装</a>";
-    var strCLodopInstall_2 = "<br>（若此前已安装过，可<a href='CLodop.protocol:setup' target='_self'>点这里直接再次启动</a>）";
-    var strCLodopInstall_3 = "，成功后请刷新本页面。</font>";
-    var strCLodopUpdate = "<br><font color='#FF00FF'>Web打印服务CLodop需升级!点击这里<a href='CLodop_Setup_for_Win32NT.zip' target='_self'>执行升级</a>,升级后请刷新页面。</font>";
+    var strFontTag= "<br><font color='#FF00FF'>打印控件";
+    var strLodopInstall   = strFontTag+"未安装!点击这里<a href='install_lodop32.zip' target='_self'>执行安装</a>";
+    var strLodopUpdate    = strFontTag+"需要升级!点击这里<a href='install_lodop32.zip' target='_self'>执行升级</a>";
+    var strLodop64Install = strFontTag+"未安装!点击这里<a href='install_lodop64.zip' target='_self'>执行安装</a>";
+    var strLodop64Update  = strFontTag+"需要升级!点击这里<a href='install_lodop64.zip' target='_self'>执行升级</a>";
+    var strCLodopInstallA = "<br><font color='#FF00FF'>Web打印服务CLodop未安装启动，点击这里<a href='CLodop_Setup_for_Win32NT.zip' target='_self'>下载执行安装</a>";
+    var strCLodopInstallB = "<br>（若此前已安装过，可<a href='CLodop.protocol:setup' target='_self'>点这里直接再次启动</a>）";
+    var strCLodopUpdate   = "<br><font color='#FF00FF'>Web打印服务CLodop需升级!点击这里<a href='CLodop_Setup_for_Win32NT.zip' target='_self'>执行升级</a>";
+    var strLodop7FontTag    = "<br><font color='#FF00FF'>Web打印服务Lodop7";
+    var strLodop7HrefX86    = "点击这里<a href='Lodop7_Linux_X86_64.tar.gz' target='_self'>下载安装</a>(下载后解压，点击lodop文件开始执行)";
+    var strLodop7HrefARM    = "点击这里<a href='Lodop7_Linux_ARM64.tar.gz'  target='_self'>下载安装</a>(下载后解压，点击lodop文件开始执行)";
+    var strLodop7Install_X86 = strLodop7FontTag+"未安装启动，"+strLodop7HrefX86;
+    var strLodop7Install_ARM = strLodop7FontTag+"未安装启动，"+strLodop7HrefARM;
+    var strLodop7Update_X86  = strLodop7FontTag+"需升级，"+strLodop7HrefX86;
+    var strLodop7Update_ARM  = strLodop7FontTag+"需升级，"+strLodop7HrefARM;
+    var strInstallOK = "，成功后请刷新本页面或重启浏览器。</font>";
     var LODOP;
     try {
-        var ua = navigator.userAgent;
-        var isIE = !!(ua.match(/MSIE/i)) || !!(ua.match(/Trident/i));
-        if (needCLodop()) {
+        var isWinIE    = (/MSIE/i.test(navigator.userAgent)) || (/Trident/i.test(navigator.userAgent));
+        var isWinIE64  = isWinIE && (/x64/i.test(navigator.userAgent));
+        var isLinuxX86 = (/Linux/i.test(navigator.platform)) && (/x86/i.test(navigator.platform));
+        var isLinuxARM = (/Linux/i.test(navigator.platform)) && (/aarch/i.test(navigator.platform));
+
+        if (needCLodop() || isLinuxX86 || isLinuxARM) {
             try {
                 LODOP = getCLodop();
             } catch (err) {}
             if (!LODOP && CLodopJsState !== "complete") {
-                if (CLodopJsState == "loading") alert("网页还没下载完毕，请稍等一下再操作."); else alert("没有加载CLodop的主js，请先调用loadCLodop过程.");
+                if (CLodopJsState == "loading") alert("网页还没下载完毕，请稍等一下再操作."); else alert("未曾加载Lodop主JS文件，请先调用loadCLodop过程.");
                 return;
             }
+            var strAlertMessage;
             if (!LODOP) {
-                document.body.innerHTML = strCLodopInstall_1 + (CLodopIsLocal ? strCLodopInstall_2 : "") + strCLodopInstall_3 + document.body.innerHTML;
+                if (isLinuxX86) strAlertMessage = strLodop7Install_X86; else
+                if (isLinuxARM) strAlertMessage = strLodop7Install_ARM; else
+                strAlertMessage = strCLodopInstallA + (CLodopIsLocal ? strCLodopInstallB : "") ;
+                document.body.innerHTML = strAlertMessage + strInstallOK + document.body.innerHTML;
                 return;
             } else {
-                if (CLODOP.CVERSION < "4.0.9.9") {
-                    document.body.innerHTML = strCLodopUpdate + document.body.innerHTML;
-                }
-                if (oEMBED && oEMBED.parentNode)
-                    oEMBED.parentNode.removeChild(oEMBED);
-                if (oOBJECT && oOBJECT.parentNode)
-                    oOBJECT.parentNode.removeChild(oOBJECT);
+                if (isLinuxX86 && LODOP.CVERSION < "7.0.3.9") strAlertMessage = strLodop7Update_X86; else
+                if (isLinuxARM && LODOP.CVERSION < "7.0.3.9") strAlertMessage = strLodop7Update_ARM; else
+                if (CLODOP.CVERSION < "4.1.5.5")              strAlertMessage = strCLodopUpdate;
+
+                if (strAlertMessage) document.body.innerHTML = strAlertMessage+ strInstallOK + document.body.innerHTML;
             }
-        } else {
-            var is64IE = isIE && !!(ua.match(/x64/i));
-            //==如果页面有Lodop就直接使用,否则新建:==
+        } else { //==如果页面有Lodop插件就直接使用,否则新建:==
             if (oOBJECT || oEMBED) {
-                if (isIE)
+                if (isWinIE)
                     LODOP = oOBJECT;
                 else
                     LODOP = oEMBED;
@@ -111,7 +119,7 @@ function getLodop(oOBJECT, oEMBED) {
                 LODOP.setAttribute("width", 0);
                 LODOP.setAttribute("height", 0);
                 LODOP.setAttribute("style", "position:absolute;left:0px;top:-100px;width:0px;height:0px;");
-                if (isIE)
+                if (isWinIE)
                     LODOP.setAttribute("classid", "clsid:2105C259-1E0C-4534-8141-A753534CB4CA");
                 else
                     LODOP.setAttribute("type", "application/x-print-lodop");
@@ -121,21 +129,18 @@ function getLodop(oOBJECT, oEMBED) {
                 LODOP = CreatedOKLodopObject;
             //==Lodop插件未安装时提示下载地址:==
             if ((!LODOP) || (!LODOP.VERSION)) {
-                if (ua.indexOf('Chrome') >= 0)
-                    document.body.innerHTML = strHtmChrome + document.body.innerHTML;
-                if (ua.indexOf('Firefox') >= 0)
-                    document.body.innerHTML = strHtmFireFox + document.body.innerHTML;
-                document.body.innerHTML = (is64IE ? strHtm64_Install : strHtmInstall) + document.body.innerHTML;
+                document.body.innerHTML = (isWinIE64 ? strLodop64Install : strLodopInstall) + strInstallOK + document.body.innerHTML;
                 return LODOP;
             }
+            if (LODOP.VERSION < "6.2.2.6") {
+                document.body.innerHTML = (isWinIE64 ? strLodop64Update : strLodopUpdate) + strInstallOK + document.body.innerHTML;
+            }
         }
-        if (LODOP.VERSION < "6.2.2.6") {
-            if (!needCLodop())
-                document.body.innerHTML = (is64IE ? strHtm64_Update : strHtmUpdate) + document.body.innerHTML;
-        }
-        //===如下空白位置适合调用统一功能(如注册语句、语言选择等):==
-   
-        //=======================================================
+        //===如下空白位置适合调用统一功能(如注册语句、语言选择等):=======================
+
+        LODOP.SET_LICENSES("","13528A153BAEE3A0254B9507DCDE2839","EDE92F75B6A3D917F65910","D60BC84D7CF2DE18156A6F88987304CB6D8");
+
+        //===============================================================================
         return LODOP;
     } catch (err) {
         alert("getLodop出错:" + err);
