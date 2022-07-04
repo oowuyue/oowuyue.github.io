9289b5d00047c1cb823c2c85e84f210af1a00454
oowuyue <851616860@qq.com>
2022-07-04 16:58:56 +0800

    Add files via upload

--- LodopFuncs.js
+++ LodopFuncs.js
@@ -1,4 +1,4 @@
-//==本JS是加载Lodop插件或Web打印服务CLodop/Lodop7的综合示例，可直接使用，建议理解后融入自己程序==
+﻿//==本JS是加载Lodop插件或Web打印服务CLodop/Lodop7的综合示例，可直接使用，建议理解后融入自己程序==
 
 //用双端口加载主JS文件Lodop.js(或CLodopfuncs.js兼容老版本)以防其中某端口被占:
 const MainJS ="CLodopfuncs.js";
@@ -72,21 +72,21 @@ function checkOrTryHttp() {
 (function loadCLodop(){
   if (!needCLodop()) return;
   CLodopIsLocal = !!((URL_WS1 + URL_WS2).match(/\/\/localho|\/\/127.0.0./i));
-
   LoadJsState = "loadingA";
   if (!window.WebSocket && window.MozWebSocket) window.WebSocket=window.MozWebSocket;
-  if (!window.WebSocket) return(checkOrTryHttp());
   //ws方式速度快(小于200ms)且可避免CORS错误,但要求Lodop版本足够新:
-  try { var WSK1=new WebSocket(URL_WS1);}
-  catch (err) {console.log(err) ;setTimeout("checkOrTryHttp();",200); }
- 
-  WSK1.onopen = function(e) { setTimeout("checkOrTryHttp();",200); }
-  WSK1.onmessage = function(e) {if (!window.CLODOP2015_7028) eval(e.data);}
-  WSK1.onerror = function(e) {
-       var WSK2=new WebSocket(URL_WS2);
-       WSK2.onopen = function(e) {setTimeout("checkOrTryHttp();",200);}
-       WSK2.onmessage = function(e) {if (!window.CLODOP2015_7028) eval(e.data);}
-       WSK2.onerror= function(e) {checkOrTryHttp();}
+  try {
+    var WSK1=new WebSocket(URL_WS1);
+    WSK1.onopen = function(e) { setTimeout("checkOrTryHttp();",200); }
+    WSK1.onmessage = function(e) {if (!window.CLODOP2015_7028) eval(e.data);}
+    WSK1.onerror = function(e) {
+         var WSK2=new WebSocket(URL_WS2);
+         WSK2.onopen = function(e) {setTimeout("checkOrTryHttp();",200);}
+         WSK2.onmessage = function(e) {if (!window.CLODOP2015_7028) eval(e.data);}
+         WSK2.onerror= function(e) {checkOrTryHttp();}
+    }
+  } catch(e){
+    checkOrTryHttp();
   }
 })();
 
@@ -185,61 +185,3 @@ function getLodop(oOBJECT, oEMBED) {
     }
 }
 
-
-/*
-
-
-                           http                                                     https
-
-
-
-                                                 Mixed Content错误
-
-                    http://localhost:8000/                                  https://localhost:8443/               
-                                                                                                               Chrome 84 不支持 TLS 1. 1                
-                                                                            https://localhost.lodop.net:8443/            
-
-
-
-                    Chrome94版阻断http页面访问localhost服务
-                    Block insecure private network requests错误  
-                    解决方法  1 改chrome设置block  2 网站改成https
-                             3 clodop通过webcosk加载js  
--------------------------------------------------------------------------------------------------------------------------------------------------
-                      
-                    网站是http                                                网站是https
-    
-            测试网址1 http://hkb818ae00.youhui112.com/index.html            测试网址1 https://oowuyue.github.io/index.html
-                加载http8000                                                     加载http8000
-
-            测试网址2 http://hkb818ae00.youhui112.com/index2.html           测试网址2 https://oowuyue.github.io/index2.html
-                加载https8443                                                    加载https8443
-        
-
-       chrome105                                                                    
-       Edge103      都无法访问 报Block insecure private 错误                     都可以访问
-       QQ 94内核
-
-
-   360急速86内核     都可以访问                                                  都可以访问
-
-                                                                                                                                                                        
-        火狐102      都可以访问                                                  都可以访问
- 
-
-    火狐52(麒麟)     都可以访问                                                  http8000无法访问 报Mixed Content错误   直接ws8000 报The operation is insecure.
-                                                                                https8443可以访问                     直接wss8443不报错但返回0字节
-                                                                               
-
-                                                                               https://developer.mozilla.org/en-US/docs/Web/Security/Mixed_content             
-                                                                               https://support.mozilla.org/zh-CN/kb/%E5%A6%82%E4%BD%95%E9%92%88%E5%AF%B9%E5%AE%89%E5%85%A8%E7%BD%91%E7%AB%99%E7%9A%84%E9%94%99%E8%AF%AF%E7%A0%81%E8%BF%9B%E8%A1%8C%E6%95%85%E9%9A%9C%E6%8E%92%E9%99%A4
-
-                                                                               
-               
-                                                                                
-         
-
-                   
-                 
-                  
- */  
