6f5080a9c6c5449aad8b8719a818cbffdaa26f7b
oowuyue <oowuyue@gmail.com>
2023-11-20 16:29:10 +0800

    ii

--- quant/test.py
+++ quant/test.py
@@ -1,18 +1,3 @@
-a = 1
+a = (1,2,3)
 
-def change_integer(a):
-    a = a + 1
-    return a
-
-print(change_integer(a))
-print(a)
-
-
-b = [1,2,3]
-
-def change_list(b):
-    b[0] = b[0] + 1
-    return b
-
-print(change_list(b))
-print(b)
+print(dir(map))
